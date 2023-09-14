867bec964da535fc09505426df2bb5a0f84e5501
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-09-14 16:36:39 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script >
+    import HomePage from './pages/HomePage.vue';
+    // import AppHeader from './components/AppHeader.vue';
+
+    export default{
+      components :{
+        HomePage,
+        // AppHeader,
+      }
+     }
+
+
+</script>
+
+<template>
+
+  <div>
+    <!-- <AppHeader/> -->
+    <HomePage/>
+  </div>
+  
+
+</template>
+
+<style  lang="scss">
+@use './styles/generals.scss' as *
+
+</style>
