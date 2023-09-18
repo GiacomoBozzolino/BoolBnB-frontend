0b476e9596e70b671aca033aae12a767921278d8
EugeniuPeruzzi <jaka1997@live.it>
2023-09-18 11:38:23 +0200

    Agiunta del footer

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,31 @@
+<script>
+
+export default {
+    name: 'AppFooter',
+
+    data() {
+        return {
+
+        }
+    },
+
+
+}
+
+</script>
+
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1>Io saro un Footer</h1>
+            
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
