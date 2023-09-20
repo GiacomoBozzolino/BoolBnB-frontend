b07a2a51629635980ad82ece48dbec8e8498d3a5
SteCard <stefanocardillo14@gmail.com>
2023-09-20 03:01:01 +0200

    [FIX] small fix

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -13,12 +13,12 @@
 
   <div class="container">
     <div class="row">
-      <div class="col-12 col-md-6 d-flex  align-items-center">
+      <div class="col-12 col-md-6 d-flex justify-content-end align-items-center">
         <!-- Aggiungo col-12 col-md-6 per occupare 6 colonne su schermi medi e grandi -->
-        <div class="input-group justify-content-end">
+        <div class="input-group">
           <input type="search" class="form-control rounded" v-model="input" name="apartment-search" id="apartment-search" placeholder="Cerca un appartamento">
           <div class="input-group-append">
-              <button class="btn btn-primary" type="button">Cerca</button>
+              <button class="btn btn-warning" type="button">Cerca</button>
           </div>
         </div>
       </div>
