71c8780ce2ac35510801dc5296f0dcf4b66870dc
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-09-25 13:29:14 +0200

    [FEAT]aggiunto controllo

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -8,13 +8,17 @@ export default {
   data() {
     return {
       store,
+      searchCity:'',
     };
   },
 
+
+
+
   methods: {
     async searchApartment(city) {
-            try {
-                const response = await axios.get(`http://localhost:8000/api/search`, {
+            if(this.searchCity !== ''){
+              const response = await axios.get(`http://localhost:8000/api/search`, {
                 params: {
                     city: city,
                 },
@@ -27,14 +31,15 @@ export default {
 
                 // Reindirizzamento
                 this.$router.push({ name: 'Home' });
-            } catch (error) {
-                console.error('Errore durante la ricerca:', error);
+            } 
+
             }
-    
+                
+
+          
   },
 }
 
-}
 </script>
 
 
@@ -46,13 +51,15 @@ export default {
       <div class="col-12 col-md-6 d-flex justify-content-end align-items-center">
         <form @submit.prevent="searchApartment($event.target.city.value)">
             <div class="form-floating mb-3 col-5 col-lg-8 mx-auto ms-lg-0">
-                <input type="text" class="form-control text-dark" id="city" placeholder="Roma" name="city">
+                <input type="text" class="form-control text-dark" id="city" placeholder="Roma" name="city" v-model="searchCity">
                 <label name="city" class="text-dark" for="floatingInput">Città</label>
             </div>
             <!-- bottone cerca -->
-            <div class="d-flex d-lg-block justify-content-center py-2 py-lg-0">
-                <input type="submit" value="Cerca" class="mx-md-auto btn btn-lg my_btn">
+            <div class="d-flex d-lg-block justify-content-center py-2 py-lg-0"  >
+                <button type="submit" value="Cerca" class="btn btn-primary" > cerca
+                </button>
             </div>
+
         </form>
       </div>
     </div>
