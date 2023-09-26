82458567777c873b2202cb58d9582481013d37ff
EugeniuPeruzzi <jaka1997@live.it>
2023-09-26 15:41:32 +0200

    changed distance to range

--- src/pages/AdvancedSearch.vue
+++ src/pages/AdvancedSearch.vue
@@ -1,5 +1,5 @@
+
 <script>
-// importo stro e aCSios dove si trova la base API
 import { store } from "../store";
 import axios from "axios";
 import AppLoader from '../components/AppLoader.vue';
@@ -8,147 +8,139 @@ import RenderApartments from "../components/RenderApartments.vue";
 
 export default {
   name: "AdvancedSearch",
-  components:{
+  components: {
     AppLoader,
     SearchBar,
     RenderApartments,
-},
-
-data() {
+  },
+  data() {
     return {
       searchCity: store.searchCity || this.$route.params.searchCity ||  '',
       n_rooms: this.$route.params.n_rooms || '',
       n_beds: this.$route.params.n_beds || '',
-      distance: this.$route.params.distance || '',
+      distance: this.$route.params.distance || 0,
       suggestions: [],
       store,
+
+      // Range
+
     }
-
   },
-
-
-  computed:{
-    //FUNZIONE FILTRAGGIO <-------FUNZIONA
+  computed: {
+    // Funziona questa??
     filteredApartments() {
       return store.apartments.filter((apartments) => {
         const query = store.searchApartments.toLowerCase();
-        // console.log(query);
-        // Esegui una ricerca per nome, città o numero di stanze
-        return (
-          apartments.title.toLowerCase().includes(query)
-        );
-
+        return apartments.title.toLowerCase().includes(query);
       });
     },
-  },
-
-
-  methods: {
     
-    // FUNZIONE RICERCA SUGGERIMENTI
+    // Range
+    rangeValue() {
+      return `${this.distance}km`;
+    },
+
+  },
+  methods: {
     async searchSuggestions() {
       if (this.searchCity !== '') {
-          const response = await axios.get(`https://api.tomtom.com/search/2/search/${this.searchCity}.json?key=zXBjzKdSap3QJnfDcfFqd0Ame7xXpi1p&language=it-IT&idxSets=Str&countrySet=IT&typeahead=true`, {
-          });
-
-          this.suggestions = response.data.results;
-        } 
-       else {
+        const response = await axios.get(`https://api.tomtom.com/search/2/search/${this.searchCity}.json?key=zXBjzKdSap3QJnfDcfFqd0Ame7xXpi1p&language=it-IT&idxSets=Str&countrySet=IT&typeahead=true`, {
+        });
+        this.suggestions = response.data.results;
+      } else {
         this.suggestions = [];
       }
     },
-
-// FUNZIONE DI RICERCA INDIRIZZO NEL DB
     async searchAdvancedApartment(city) {
       if (this.searchCity !== '') {
-          const response = await axios.get(`http://localhost:8000/api/searchAdvanced`, {
-            params: {
-              city: city,
-              n_rooms:this.n_rooms,
-              n_beds:this.n_beds,
-              distance:this.distance,
-            },
-          });
-
-          store.apartments = response.data;
-          store.city = city;
-
-          this.$router.push({ name: 'AdvancedSearch', 
-          params: { 
+        const response = await axios.get(`http://localhost:8000/api/searchAdvanced`, {
+          params: {
+            city: city,
+            n_rooms: this.n_rooms,
+            n_beds: this.n_beds,
+            distance: this.distance,
+          },
+        });
+        store.apartments = response.data;
+        store.city = city;
+        this.$router.push({
+          name: 'AdvancedSearch',
+          params: {
             searchCity: `${this.searchCity}`,
-            distance:  `${this.distance}`,
-            n_rooms: `${this.n_rooms }`,
-            n_beds:  `${this.n_beds}`,
-          } });
-        } 
-       
+            distance: `${this.distance}`,
+            n_rooms: `${this.n_rooms}`,
+            n_beds: `${this.n_beds}`,
+          }
+        });
+      }
     },
-
-    // FUNZIONE SELEZIONE INDIRIZZO DAI SUGGERIMENTI
     selectSuggestion(suggestion) {
       this.searchCity = suggestion.address.freeformAddress;
       this.suggestions = [];
       console.log(this.searchCity)
     },
   },
-
 };
 </script>
 
 <template>
   <div class="container">
-    <div class="row">
-   
-        <div>
-    <form @submit.prevent="searchAdvancedApartment(searchCity)" autocomplete="off">
-      <div class="form-floating mb-3 col-5 col-lg-8 mx-auto ms-lg-0">
-        <input
-          type="text"
-          class="form-control text-dark"
-          id="city"
-          placeholder="Roma"
-          v-model="searchCity"
-          @input="searchSuggestions"
-        >
-        <label class="text-dark" for="city">Città</label>
-      </div>
-      <!-- Suggerimenti -->
-      <div v-if="suggestions.length > 0" class="suggestions">
-        <ul>
-          <li v-for="suggestion in suggestions" :key="suggestion.id" @click="selectSuggestion(suggestion)">
-            {{ suggestion.address.freeformAddress }}
-          </li>
-        </ul>
-      </div>
-
-      <div class="col-11 p-2">
+    <div class="row ">
+      <!-- FORM CONTAINER -->
+      <div class="form-container">
+        <form @submit.prevent="searchAdvancedApartment(searchCity)" autocomplete="off">
+          <!-- SEARCH CONTAINER -->
+          <div class="serach-container col-3">
+            <div class="d-flex align-items-center">
+              <label class="text-dark me-2" for="city">Città</label>
+              <input
+                type="text"
+                class="form-control text-dark"
+                id="city"
+                placeholder="Roma"
+                v-model="searchCity"
+                @input="searchSuggestions"
+              >
+            </div>
+                      <!-- Suggerimenti -->
+          <div v-if="suggestions.length > 0" class="suggestions">
+            <ul>
+              <li v-for="suggestion in suggestions" :key="suggestion.id" @click="selectSuggestion(suggestion)">
+                {{ suggestion.address.freeformAddress }}
+              </li>
+            </ul>
+          </div>
+          </div>
+          <!-- RANGE SEARCH -->
+          <div class="col-5 d-flex align-items-center justify-content-between mt-4 px-2">
             <label for="distance" class="form-label">Imposta raggio di ricerca</label>
-            <input type="text" class="form-control" v-model="distance" id="distance" name="n_rooms"
-            min="1" max="50">
-            
+            <div class="range-container d-flex">
+              <span class="me-3">{{ rangeValue}}  </span>
+              <input type="range" class="form-range border rounded-5 p-2" v-model="distance" id="distance" name="n_rooms" min="1" max="300">
+            </div>
+          </div>
+
+          <!-- RANGE ROOMS -->
+          <div class="d-flex">
+            <div class="col-2 p-2">
+              <label for="n_rooms" class="form-label">Numero Stanze <font-awesome-icon :icon="['fas', 'house']" /></label>
+              <input type="number" class="form-control" v-model="n_rooms" id="n_rooms" name="n_rooms" min="1" max="15" >
+            </div>
+            <!-- RANGE BEDS -->
+            <div class="col-2 p-2">
+              <label for="n_beds" class="form-label">Numero Letti <font-awesome-icon :icon="['fas', 'house']" /></label>
+              <input type="number" class="form-control" v-model="n_beds" id="n_beds" name="n_beds" min="1" max="15">
+            </div>
+          </div>
+
+          <!-- Bottone Cerca -->
+          <button type="submit" class="btn btn-primary" :disabled="searchCity === ''">Cerca</button>
+
+        </form>
       </div>
-      <div class="d-flex">
-        <div class="col-6 p-2">
-            <label for="n_rooms" class="form-label">Numero Stanze <font-awesome-icon :icon="['fas', 'house']" /></label>
-            <input type="number" class="form-control" v-model="n_rooms" id="n_rooms" name="n_rooms"
-            min="1" max="15">
-        </div>
-        <div class="col-6 p-2">
-            <label for="n_beds" class="form-label">Numero Letti <font-awesome-icon :icon="['fas', 'house']" /></label>
-            <input type="number" class="form-control" v-model="n_beds" id="n_beds" name="n_beds"
-            min="1" max="15">
-        </div>
-      </div>
-      
 
-      <!-- Bottone Cerca -->
-      <button type="submit" class="btn btn-primary" :disabled="searchCity === '' " >Cerca</button>
-
-    </form>
-  </div>
-
-        <div class="col-12 d-flex flex-wrap my-4">
-        <div class="card m-3" style="width: 23rem; " v-for="(apartment, index) in filteredApartments" :key="index">
+      <div class="col-12 d-flex flex-wrap my-4">
+        <div class="card m-3" style="width: 23rem;" v-for="(apartment, index) in filteredApartments" :key="index">
           <div class="card-image-top">
             <!-- da sistemare lo storage -->
             <img :src="`${store.apartmentsUrl}/storage/${apartment.cover_img}`" class="img-fluid" />
@@ -156,10 +148,10 @@ data() {
           <div class="card-body">
             <h4 class="card-title text-center">{{ apartment.title }}</h4>
             <div class="description-card overflow-auto mt-2">
-                <p class="card-text py-2 text-start">
-                <i class="fa-solid fa-location-dot"></i> Indirizzo: 
+              <p class="card-text py-2 text-start">
+                <i class="fa-solid fa-location-dot"></i> Indirizzo:
                 <strong>{{ apartment.address }}</strong>
-              </p> 
+              </p>
               <p class="card-text py-2 text-start">
                 <i class="fa-solid fa-person-shelter"></i> Numero camere:
                 <strong>{{ apartment.n_rooms }}</strong>
@@ -179,14 +171,14 @@ data() {
               <p class="card-text py-2 text-start">
                 <i class="fa-solid fa-ruler-combined"></i> Metri quadri:
                 <strong>{{ apartment.square_meters }}</strong>
-              </p> 
+              </p>
             </div>
           </div>
           <div class="card-footer">
             <router-link class="btn btn-sm btn-primary" :to="{name:'SingleApartment', params:{slug:apartment.slug}}">Guarda il progetto</router-link>
           </div>
         </div>
-      </div>    
+      </div>
     </div>
   </div>
 </template>
@@ -203,25 +195,25 @@ data() {
   max-height: 150px;
   overflow-y: auto;
 }
-
 .suggestions ul {
   list-style-type: none;
   padding: 0;
   margin: 0;
 }
-
 .suggestions li {
   cursor: pointer;
   padding: 5px;
   border-bottom: 1px solid #ccc;
 }
-
 .suggestions li:hover {
   background-color: #f0f0f0;
 }
-
-
-.btn:disabled{
+.btn:disabled {
   background-color: gray;
+}
+
+// SEARCH FORM
+.padding-search{
+  padding-left: 100px;
 }
 </style>
